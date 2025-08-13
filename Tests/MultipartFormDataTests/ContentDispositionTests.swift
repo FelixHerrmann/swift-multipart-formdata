7e07cc9d22da1011abad6e65f07ec249ffe238ea
Felix Herrmann <felix.herrmann@icloud.com>
2025-08-13 15:29:30 +0200

    Skip platform-specific test if needed

--- Tests/MultipartFormDataTests/ContentDispositionTests.swift
+++ Tests/MultipartFormDataTests/ContentDispositionTests.swift
@@ -9,28 +9,36 @@ import XCTest
 @testable import MultipartFormData
 
 final class ContentDispositionTests: XCTestCase {
-    func testPercentEncodingError() throws {
+    func testUncheckedInitValid() {
         XCTAssertNoThrow(try ContentDisposition(uncheckedName: "a", uncheckedFilename: "a"))
-        
+    }
+    
+    func testUncheckedInitInvalid() throws {
         // https://stackoverflow.com/questions/33558933/why-is-the-return-value-of-string-addingpercentencoding-optional
-        // does not work on Linux, can still encoding there
-        let nonPercentEncodableString = try XCTUnwrap(String(bytes: [0xD8, 0x00] as [UInt8], encoding: .utf16BigEndian))
-        if nonPercentEncodableString.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) == nil {
-            XCTAssertThrowsError(try ContentDisposition(uncheckedName: nonPercentEncodableString, uncheckedFilename: nil))
-            XCTAssertThrowsError(try ContentDisposition(uncheckedName: "", uncheckedFilename: nonPercentEncodableString))
+        let bytes: [UInt8] = [0xD8, 0x00]
+        
+        // Ensure the non-encodable string can be created, e.g. on iOS 18 it no longer works.
+        guard let nonPercentEncodableString = String(bytes: bytes, encoding: .utf16BigEndian) else {
+            throw XCTSkip("UTF16 byte encoding failed")
         }
+        
+        // Ensure the percent-encoding fails on the current platform, e.g. on Linux it encodes.
+        guard nonPercentEncodableString.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) == nil else {
+            throw XCTSkip("percent encoding didn't fail")
+        }
+        
+        XCTAssertThrowsError(try ContentDisposition(uncheckedName: nonPercentEncodableString, uncheckedFilename: nil))
+        XCTAssertThrowsError(try ContentDisposition(uncheckedName: "", uncheckedFilename: nonPercentEncodableString))
     }
     
     func testParameters() throws {
         let contentDisposition = ContentDisposition(name: "a", filename: "a")
-        
         XCTAssertEqual(contentDisposition.parameters[0], HTTPHeaderParameter("name", value: "a"))
         XCTAssertEqual(contentDisposition.parameters[1], HTTPHeaderParameter("filename", value: "a"))
     }
     
     func testData() throws {
         let contentDisposition = ContentDisposition(name: "a", filename: "a")
-        
         XCTAssertEqual(contentDisposition._data, Data("Content-Disposition: form-data; name=\"a\"; filename=\"a\"".utf8))
     }
 }
